59f576cf71dc1611841a14ce9b91e17d68673486
Cendeal <5205093You>
2020-07-27 16:33:00 +0800

    [word frequency game][cendeal]refactor: rename w to word,i to count

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -2,9 +2,9 @@ public class Word {
     private final String value;
     private final int count;
 
-    public Word(String w, int i) {
-        this.value = w;
-        this.count = i;
+    public Word(String word, int count) {
+        this.value = word;
+        this.count = count;
     }
 
     public String getValue() {
